0d41b9d17cf11babc577a46ebc7ed6dee3bbb12e
LucyNguku <lucynguku901@gmail.com>
2022-10-19 13:29:15 -0700

    task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,36 +1,19 @@
-#include "main.h"
-
+#include "main."
 /**
-<<<<<<< HEAD
- * main - print _putchar
- *
- * Return: 0
-=======
- * main - prints _putchar and a new line
- * Return: Always 0
->>>>>>> 847143dd7d3a927d999f50d42367500af8068545
- */
+* main - prints _putchar and a new line
+*
+* Return: Always 0
+*/
 int main(void)
 {
-	_putchar('_');
-<<<<<<< HEAD
-	_putchar('p');
-=======
-        _putchar('p');
->>>>>>> 847143dd7d3a927d999f50d42367500af8068545
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-<<<<<<< HEAD
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
-=======
-
-	return (0);
-}
-
->>>>>>> 847143dd7d3a927d999f50d42367500af8068545
