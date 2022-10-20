4823b5e458dde437557212bbea72fae386fd217d
LucyNguku <lucynguku901@gmail.com>
2022-10-20 12:17:48 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-
 /**
- * main - Entry Point
+ * main - print a line of code using puts
  *
  * Return: Always 0 (Success) 
  */
-
 int main(void)
 {
-          puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
